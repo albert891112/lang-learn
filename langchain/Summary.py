630bee47d04c1f8ab81639f52e8cd8471e2c2c16
Albert <Albert_Yang@systemweb.com.tw>
2025-10-16 21:46:25 +0800

    ✨feature : LangSmith 測試
    新增 langSmith 追蹤

---
+++ langchain/Summary.py
@@ -0,0 +1,24 @@
+from init_model import llm
+
+from langchain_core.prompts import ChatPromptTemplate
+
+prompt_str = """
+Summarize the following text in a concise manner:
+{text}
+"""
+
+prompt = ChatPromptTemplate.from_template(prompt_str)
+
+model_with_prompt = prompt | llm
+
+
+def summarize(text: str) -> str:
+    """Summarize the given text."""
+    return model_with_prompt.invoke({"text": text}).content
+
+
+if __name__ == "__main__":
+    user_input = input("Enter text to summarize: ")
+    summary = summarize(user_input)
+    print("=============================================================")
+    print("Summary:", summary)
